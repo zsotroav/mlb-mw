f38bdea2a07a36545c92bd135dbefbb30e38e74a
zsotroav <zsotroav@gmail.com>
2022-06-14 22:22:03 +0200

    Multiplication and a few fixes

--- episode_updater.py
+++ episode_updater.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from auth import auth, URL, S
+import action_fox
+import json
 
 
 sched = BlockingScheduler()
@@ -19,7 +21,9 @@ def protect(episode):
         "reason": "BOT: The episode is scheduled to begin airing shortly."
     }
     R = S.post(URL, data=PARAMS)
-    print(R.text)
+    action_fox.send(f"Protection update attempted for \"{episode}\"",
+                    f"```json\n{json.dumps(R.text.json(), indent=2)}\n```",
+                    16711680)
 
 
 def updSpoiler(episode: str, mode: int):
@@ -37,19 +41,26 @@ def updSpoiler(episode: str, mode: int):
         "rvslots": "main"
     })
 
+    action_fox.send(f"Downloaded page for the spoiler doctrine",
+                    f"",
+                    5814783)
+
     page_cont = page.json()["query"]["pages"][0]["revisions"][0]["slots"]["main"]["content"]
 
     if mode == 0:
         page_cont = page_cont.replace(f"<span style=\"font-weight: bold; color: #ffea00;\" name=\"{episode}\">✘</span>",
                                       f"<span style=\"font-weight: bold; color: #39fc03;\" name=\"{episode}\">✓</span>")
+        to = "OK"
     elif mode == 1:
         page_cont = page_cont.replace(f"<span name=\"{episode}\">✘</span>",
                                       f"<span style=\"font-weight: bold; color: #ffea00;\" name=\"{episode}\">✘</span>")
         page_cont = page_cont.replace(f"<span style=\"font-weight: bold; color: #ff0000;\" name=\"{episode}\">✘</span>",
                                       f"<span style=\"font-weight: bold; color: #ffea00;\" name=\"{episode}\">✘</span>")
+        to = "CD"
     else:
         page_cont = page_cont.replace(f"<span name=\"{episode}\">✘</span>",
                                       f"<span style=\"font-weight: bold; color: #ff0000;\" name=\"{episode}\">✘</span>")
+        to = "WP"
 
     PARAMS = {
         "action": "edit",
@@ -60,10 +71,12 @@ def updSpoiler(episode: str, mode: int):
         "text": page_cont
     }
     R = S.post(URL, data=PARAMS)
-    print(R.text)
+    action_fox.send(f"Spoiler doctrine update attempted for \"{episode}\", to:{to}",
+                    f"```json\n{json.dumps(R.text.json(), indent=2)}\n```",
+                    16711680)
 
 
-sched.add_job(protect, 'date', run_date=datetime(2022, 6, 14, 0, 30, 00), args=['Evolution'])
-sched.add_job(updSpoiler, 'date', run_date=datetime(2022, 6, 14, 0, 30, 00), args=['Evolution', 0])
+sched.add_job(protect, 'date', run_date=datetime(2022, 6, 21, 0, 30, 00), args=['Multiplication'])
+sched.add_job(updSpoiler, 'date', run_date=datetime(2022, 6, 21, 0, 30, 00), args=['Multiplication', 2])
 
 sched.start()
